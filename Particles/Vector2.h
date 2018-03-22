bfb02df6230c9a1f8c0b051ac1d33ba3e2ba3550
Krivosheina Nadezhda <36559276+Torire@users.noreply.github.com>
2018-03-23 01:48:00 +0300

    Add files via upload

---
+++ Particles/Vector2.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+
+const float Pi = 3.14159;
+
+struct Vector2
+{
+	float x, y;
+
+	Vector2(float, float);
+	//Конструктор и Деструктор
+	Vector2(); 
+
+	~Vector2();
+	//Произведение и деление на число
+	Vector2 operator*(float) const;
+	
+	Vector2 operator/(float) const;
+	//Обработка ввода и вывода
+	std::ostream& operator<<(std::ostream&);
+
+	std::istream& operator>>(std::istream&);
+	//Классические операторы
+	Vector2 operator+(const Vector2&) const;
+	
+	Vector2& operator+=(const Vector2& );
+
+	Vector2& operator-=(const Vector2& );
+
+	Vector2 operator-(const Vector2& ) const;
+	//Векторное произведение
+	float operator^(const Vector2&) const;
+	//Скалярное произведение
+	float operator*(const Vector2& ) const;
+	//Длина вектора
+	float Len() const;
+	//Квадрат длины
+	float SquareLen() const;
+	//Единичный вектор
+	Vector2 norm();
+	//Перпандикуляр
+	Vector2 perp();
+	//Повороты
+	Vector2& rotate(float);
+	
+	Vector2 getRotated(float);
+
+};
