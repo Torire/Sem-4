ab31d7eb169e462b8deafad7d125b9472c8fb5a6
Vlad <vlad.miryaha@gmail.com>
2018-04-13 22:54:57 +0300

    fixit

--- Vector2/Vector2.h
+++ Vector2/Vector2.h
@@ -21,6 +21,10 @@ struct Vector2
 	
 	Vector2 operator/(float) const;
 	
+	// fixit: ой, вверх ногами операторы << и >> получились
+	// вынесите эти два метода наружу из структуры -> обычные ф-и 
+	// std::ostream& operator<<(std::ostream&, const Vector2& v);
+	// std::istream& operator>>(std::istream&, Vector2& v)
 	std::ostream& operator<<(std::ostream&);
 
 	std::istream& operator>>(std::istream&);
@@ -35,16 +39,19 @@ struct Vector2
 
 	float operator*(const Vector2& ) const;
 
+	// fixit: названия методов должны быть в одном стиле ... возможно len и squareLen
+	// лишь бы везде одинаково
 	float Len() const;
 	
 	float SquareLen() const;
 	
+	// fixit: посмотрите, как у вас сделаны rotate и getRotated. видимо norm и perp надо в том же стиле сделать
 	Vector2 norm();
-	
 	Vector2 perp();
 	
 	Vector2& rotate(float);
 	
+	// fixit: если вы возвращаете Vector2, то метод должен быть константный. Если Vector2& , то неконстантный
 	Vector2 getRotated(float);
 
 };
