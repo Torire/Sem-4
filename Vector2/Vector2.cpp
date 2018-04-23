7ef62874f477c6771da2de6edaedfe33b4de86ac
Krivosheina Nadezhda <36559276+Torire@users.noreply.github.com>
2018-04-23 21:20:02 +0300

    Update Vector2.cpp

--- Vector2/Vector2.cpp
+++ Vector2/Vector2.cpp
@@ -1,137 +1,128 @@
 #include "Vector2.h"
 
-	/*
-	лучше использовать списки инициализации, если есть возможность
-	Vector2::Vector2() : x(0), y(0)
-	{
-	}
-	*/
-	Vector2::Vector2()
-	{
-	x = 0;
-	y = 0;
-	}
+/*
+лучше использовать списки инициализации, если есть возможность
+Vector2::Vector2() : x(0), y(0)
+{
+} (done?)
+*/
+Vector2::Vector2() : x(0), y(0)
+{
+}
 
-	// fixit: лучше список инициализации
-	Vector2::Vector2(float kx, float ky)
-	{
-		x = kx;
-		y = ky;
-	}
+/// компилятор сам сгенерирует такой же деструктор. нет смысла писать. лучше удалить.
+//мой компилятор ругается без этой строчки
+Vector2::~Vector2() {}
 
-	/// компилятор сам сгенерирует такой же деструктор. нет смысла писать. лучше удалить.
-	Vector2::~Vector2(){}
+// fixit: лучше список инициализации (done?)
+Vector2::Vector2(float kx, float ky) : x(kx), y(ky)
+{
+}
 
-	Vector2 Vector2::operator+(const Vector2& other) const
-	{
-		// return Vector2(x + other.x, y + other.y); ? одна строка вместо 4х
-		Vector2 result;
-		result.x = x + other.x;
-		result.y = y + other.y;
-		return result;
-	}
+Vector2 Vector2::operator+(const Vector2& other) const
+{
+	//return Vector2(x + other.x, y + other.y); ? одна строка вместо 4х (done?)
+	return Vector2(x + other.x, y + other.y);
+}
 
-	Vector2 Vector2::operator*(float k) const
-	{
-		return Vector2(x * k, y * k);
-	}
+Vector2 Vector2::operator*(float k) const
+{
+	return Vector2(x * k, y * k);
+}
+
+Vector2 Vector2::operator/(float k) const
+{
+	return Vector2(x / k, y / k);
+}
+
+// fixit: операторы << и >> надо переделать с другой сигнатурой
+std::ostream& operator<<(std::ostream& stream, const Vector2& v)
+{
+	stream << v.x << " " << v.y;
+	return stream;
+}
+
+std::istream& operator>>(std::istream& stream,  Vector2& v)
+{
+	stream >> v.x >> v.y;
+	return stream;
+}
+
+Vector2& Vector2::operator+=(const Vector2& other)
+{
+	x += other.x;
+	y += other.y;
+	return *this;
+}
+
+Vector2& Vector2::operator-=(const Vector2& other)
+{
+	x -= other.x;
+	y -= other.y;
+	return *this;
+}
+
+Vector2 Vector2::operator-(const Vector2& other) const
+{
+	// fixit: см. operator+ (done?)
+	return Vector2(x - other.x, y - other.y);
+}
+
+float Vector2::operator^(const Vector2& other) const
+{
+	return x * other.y - y * other.x;
+}
+
+float Vector2::operator*(const Vector2& other) const
+{
+	return x * other.x + y * other.y;
+}
+
+float Vector2::Len() const
+{
+	// fixit: нужно Len посчитать через squareLen, чтобы не дублировать код
+	//update
+	return sqrt(SquareLen());
+}
+
+float Vector2::SquareLen() const
+{
+	return x * x + y * y;
+}
 
 
-	// fixit: операторы << и >> надо переделать с другой сигнатурой
-	std::ostream& Vector2::operator<<(std::ostream& stream)
-	{
-		stream << x << " " << y;
-		return stream;
-	}
+Vector2 Vector2::Norm()
+{
+	// len дважны вычисляется ... вычислить корень довольно дорого (done?)
+	float len = Len();
+	return Vector2(x / len, y / len);
+}
 
-	std::istream& Vector2::operator>>(std::istream& stream)
-	{
-		stream >> x >> y;
-		return stream;
-	}
+Vector2 Vector2::Perp()
+{
+	return Vector2(y, -x);
+}
 
-	Vector2& Vector2::operator+=(const Vector2& other)
-	{
-		x += other.x;
-		y += other.y;
-		return *this;
-	}
+Vector2& Vector2::Rotate(float deg)
+{
+	/// fixit: готов поспорить, что код работает неверно в общем случае
+	/// для вертикального вектора поделим на 0 + после того, как изменения компоненты х длина изменится ... и для у уже другая длина будет использоваться, что неверно
+	//update
+	float deg0 = atan(y / x), len = Len();;
+	x = len *  cos(deg0 + deg / 180 * Pi);
+	y = len  * sin(deg0 + deg / 180 * Pi);
+	return *this;
+}
 
-	Vector2& Vector2::operator-=(const Vector2& other)
-	{
-		x -= other.x;
-		y -= other.y;
-		return *this;
-	}
-
-	Vector2 Vector2::operator-(const Vector2& other) const
-	{
-		// fixit: см. operator+
-		Vector2 result;
-		result.x = x - other.x;
-		result.y = y - other.y;
-		return result;
-	}
-
-	float Vector2::operator^(const Vector2& other) const
-	{
-		return x * other.y - y * other.x; 
-	}
-
-	float Vector2::operator*(const Vector2& other) const
-	{
-		return x * other.x + y * other.y;
-	}
-
-	float Vector2::Len() const
-	{
-		// fixit: нужно Len посчитать через squareLen, чтобы не дублировать код
-		return sqrt(x * x + y * y);
-	}
-
-	float Vector2::SquareLen() const
-	{
-		return x * x + y * y;
-	}
+Vector2 Vector2::GetRotated(float deg)
+{
+	/// fixit: у вас код дублируется ... причем с ошибкой. нужно одну через другую посчитать ф-ю 
+	//update
 	
+	float deg0 = atan(y / x), len = Len();;
+	return Vector2(len *  cos(deg0 + deg / 180 * Pi), len * sin(deg0 + deg / 180 * Pi));
+}
 
-	Vector2 Vector2::norm()
-	{
-		// len дважны вычисляется ... вычислить корень довольно дорого
-		Vector2 result;
-		result.x = x / Len();
-		result.y = y / Len();
-		return result;
-	}
-	
-	Vector2 Vector2::perp()
-	{
-		Vector2 result;
-		result.x = y;
-		result.y = -x;
-		return result;
-	}
-	
-	Vector2& Vector2::rotate(float deg)
-	{
-		/// fixit: готов поспорить, что код работает неверно в общем случае
-		./// для вертикального вектора поделим на 0 + после того, как изменения компоненты х длина изменится ... и для у уже другая длина будет использоваться, что неверно
-		float deg0 = atan (y/x);
-		x = Len() *  cos ( deg0 + deg);  
-		y =  Len() * sin ( deg0 + deg);
-		return *this;
-	}	
-	
-	Vector2 Vector2::getRotated(float deg)
-	{
-		/// fixit: у вас код дублируется ... причем с ошибкой. нужно одну через другую посчитать ф-ю
-		Vector2 result;
-		float deg0 = atan (y/x);
-		result.x = Len() *  cos ( deg0 + deg / 180 * Pi);  
-		result.y =  Len() * sin ( deg0 + deg / 180 * Pi);
-		return result;
-	}	
 
-	
 	
 	
